ec5042d2ccfb3b2920d40381b42f9eeb11d1d09c
DuzDeveloper <damianuzcanga@gmail.com>
2026-02-17 10:29:12 -0400

    wagmi fix

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -1,11 +1,13 @@
 import { http, cookieStorage, createConfig, createStorage } from 'wagmi';
 import { base } from 'wagmi/chains';
+import { farcasterMiniApp } from '@farcaster/miniapp-wagmi-connector';
 import { coinbaseWallet } from 'wagmi/connectors';
 
 export function getConfig() {
   return createConfig({
     chains: [base],
     connectors: [
+      farcasterMiniApp(),
       coinbaseWallet({
         appName: process.env.NEXT_PUBLIC_PROJECT_NAME || 'Baseman',
         preference: 'smartWalletOnly',
