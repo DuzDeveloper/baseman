9fa94edeb8844e5903c84a8c772304c651b17597
DuzDeveloper <damianuzcanga@gmail.com>
2026-02-16 22:10:42 -0400

    New

--- farcaster.config.ts
+++ farcaster.config.ts
@@ -3,26 +3,27 @@ export const ROOT_URL = process.env.NEXT_PUBLIC_URL || 'http://localhost:3000';
 export const farcasterConfig = {
   "accountAssociation": {
     "header": "eyJmaWQiOjU3NjA2MiwidHlwZSI6ImF1dGgiLCJrZXkiOiIweDRiZEUyNjkxYTRhOTg3OWQ3NjUxMjQwN0FjNmFlOThFZDU5ZTdkQzMifQ",
-    "payload": "eyJkb21haW4iOiJiYXNlY3VsdG1pbnQudmVyY2VsLmFwcCJ9",
-    "signature": "bG3ObjCp76fcDk3Y9xd1G4AIViPyRycAWQU8kV0BxClX56RHgVjO9GnQNJ0M4eWyjNRM/0u2x9rxiQvQANoeCxw=",
-},
+    "payload": "eyJkb21haW4iOiJiYXNlbWFuLWV0YS52ZXJjZWwuYXBwIn0",
+    "signature": "dM76k6w7wQSVtdDaVPHLt0LQMftgmbKOhwRsIoOojwh49nqLuzmgB2LoxX14SSDiPsKg46yx11ngNFgnG8zlsxw="
+  }
+,
   frame: {
     version: "1",
     subtitle: "Save the Basecity of the crime",
     description: "A game inspired on FlappyBirds",
-    screenshotUrls: ["https://baseman.vercel.app/images/scr1.png"],
+    screenshotUrls: ["https://baseman-eta.vercel.app/images/scr1.png"],
     name: "Baseman",
-    iconUrl: `https://baseman.vercel.app/images/icon.png`,
-    splashImageUrl: `https://baseman.vercel.app/images/splash.png`,
+    iconUrl: `https://baseman-eta.vercel.app/images/icon.png`,
+    splashImageUrl: `https://baseman-eta.vercel.app/images/splash.png`,
     splashBackgroundColor: "#1f0cd1",
-    homeUrl: "https://baseman.vercel.app",
-    webhookUrl: "https://baseman.vercel.app/api/webhook",
+    homeUrl: "https://baseman-eta.vercel.app",
+    webhookUrl: "https://baseman-eta.vercel.app/api/webhook",
     primaryCategory: "games",
     tags: ["games","flappybird","baseman"],
-    heroImageUrl: "https://baseman.vercel.app/images/heroog.png",
+    heroImageUrl: "https://baseman-eta.vercel.app/images/heroog.png",
     tagline: "",
     ogTitle: "Baseman",
     ogDescription: "A game inspired on FlappyBirds",
-    ogImageUrl: "https://baseman.vercel.app/images/heroog.png",
+    ogImageUrl: "https://baseman-eta.vercel.app/images/heroog.png",
   },
 }  as const;
