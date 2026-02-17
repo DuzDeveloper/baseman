abbf5cc74190479994996cd85be6ba49d43dba40
DuzDeveloper <damianuzcanga@gmail.com>
2026-02-16 22:21:43 -0400

    Page Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-import { BlueSkyBirdGame } from '@/components/BlueSkyBirdGame';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  other: {
+    'base:app_id': '6993d0127ca07f5750bbdc46',
+  },
+};
 
 export default function Home() {
-  return <BlueSkyBirdGame />;
+  return (<div>{/* Your page content */}</div>);
 }
