8898c277106f670ac0fc5d199a6bed53d666a514
DuzDeveloper <damianuzcanga@gmail.com>
2026-02-17 15:26:45 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,36 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { minikitConfig } from '../minikit.config';
 import Providers from "./providers";
 
 export const metadata: Metadata = {
-  title: "Baseman",
-  description: "Play Baseman and mint your NFT on Base",
+  title: 'Baseman',
+  description: 'Saving the Base Blockchain',
+    openGraph: {
+    title: minikitConfig.miniapp.ogTitle || minikitConfig.miniapp.name,
+    description: minikitConfig.miniapp.ogDescription || minikitConfig.miniapp.description,
+    images: [
+      {
+        url: minikitConfig.miniapp.ogImageUrl || minikitConfig.miniapp.heroImageUrl || minikitConfig.miniapp.iconUrl,
+        width: 1200,
+        height: 630,
+        alt: minikitConfig.miniapp.name,
+      },
+    ],
+  },
   other: {
     'base:app_id': '6993d0127ca07f5750bbdc46',
+    'fc:miniapp': JSON.stringify({
+      version: minikitConfig.miniapp.version,
+      imageUrl: minikitConfig.miniapp.heroImageUrl || minikitConfig.miniapp.iconUrl,
+      button: {
+        title: `Open ${minikitConfig.miniapp.name}`,
+        action: {
+          type: 'launch_frame',
+          url: minikitConfig.miniapp.homeUrl,
+        },
+      },
+    }),
   },
 };
 
