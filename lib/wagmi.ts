1c46f2901d1dd09a0d27686ee6fd584d141b97c4
DuzDeveloper <damianuzcanga@gmail.com>
2026-02-16 22:41:09 -0400

    wagmi update

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -1,24 +1,28 @@
-
-import { http, createConfig } from 'wagmi';
-import { baseSepolia, base } from 'wagmi/chains';
+import { http, cookieStorage, createConfig, createStorage } from 'wagmi';
+import { base } from 'wagmi/chains';
 import { coinbaseWallet } from 'wagmi/connectors';
 
-const chainId = parseInt(process.env.NEXT_PUBLIC_CHAIN_ID || '84532');
-
-// Determinar qué chain usar basado en CHAIN_ID
-export const chain = chainId === 8453 ? base : baseSepolia;
-
-// Configurar wagmi con soporte para ambas chains
-export const config = createConfig({
-  chains: [baseSepolia, base],
-  connectors: [
-    coinbaseWallet({
-      appName: process.env.NEXT_PUBLIC_PROJECT_NAME || 'NFT Mint Mini App',
+export function getConfig() {
+  return createConfig({
+    chains: [base],
+    connectors: [
+      coinbaseWallet({
+        appName: 'Baseman',
+        preference: 'smartWalletOnly',
+      }),
+    ],
+    storage: createStorage({
+      storage: cookieStorage,
     }),
-  ],
-  ssr: true,
-  transports: {
-    [baseSepolia.id]: http(),
-    [base.id]: http(),
-  },
-});
+    ssr: true,
+    transports: {
+      [base.id]: http(),
+    },
+  });
+}
+
+declare module 'wagmi' {
+  interface Register {
+    config: ReturnType<typeof getConfig>;
+  }
+}
